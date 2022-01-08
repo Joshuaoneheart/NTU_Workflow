5ef26a6efc7a252d1af4e3fc02b671d8db8399fe
matdexir <matthieudesir21@gmail.com>
2022-01-08 16:51:28 +0800

    Started to work on the left sider

--- frontend/src/Layout/layout.js
+++ frontend/src/Layout/layout.js
@@ -1,18 +1,19 @@
 import { Layout, Menu } from "antd";
 import { useState } from "react";
 import MainPage from "../Containers/MainPage/mainPage";
-import UserOutlined from '@ant-design/icons';
-import styled from "styled-components"
+import UserOutlined from "@ant-design/icons";
+import styled from "styled-components";
+import LeftSider from "../Containers/Sider/leftsider";
 
 const { Header, Footer, Sider, Content } = Layout;
 const UserIcon = styled.div`
-    border-radius:50%;
-    float: right;
-    background-color: #eee;
-    width: 50px;
-    height: 50px;
-    margin-top: 5px;
-`
+  border-radius: 50%;
+  float: right;
+  background-color: #eee;
+  width: 50px;
+  height: 50px;
+  margin-top: 5px;
+`;
 
 const CustomLayout = (props) => {
   const [showSider, setShowSider] = useState(false);
@@ -29,7 +30,9 @@ const CustomLayout = (props) => {
             background: "rgba(255, 255, 255, 0.3)",
           }}
         />
-        <UserIcon><UserOutlined/></UserIcon>
+        <UserIcon>
+          <UserOutlined />
+        </UserIcon>
       </Header>
       <Layout>
         <Sider
@@ -40,7 +43,7 @@ const CustomLayout = (props) => {
             setShowSider(collapsed);
           }}
         >
-          Left Sider
+          <LeftSider />
         </Sider>
         <Content style={{ padding: "0 50px" }}>
           <div
