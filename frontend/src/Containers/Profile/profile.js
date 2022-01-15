e0fc5bac33c5f569fb9e6b2944009cd5b812259f
matdexir <matthieudesir21@gmail.com>
2022-01-15 19:39:53 +0800

    Small modifications to the Profile modal

--- frontend/src/Containers/Profile/profile.js
+++ frontend/src/Containers/Profile/profile.js
@@ -1,4 +1,5 @@
-import { Typography, Input, Space } from "antd";
+import { Avatar, Typography, Input, Space } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
 const { Title } = Typography;
 const data = {
@@ -12,16 +13,28 @@ const data = {
 const Settings = () => {
   return (
     <>
-      <Title>{data.name}</Title>
-      <Space direction="vertical" style={{ userSelect: "none" }}>
-        <Input addonBefore="email" title="email" value={data.email} />
-        <Input addonBefore="id" title="id" value={data.id} />
-        <Input addonBefore="groups" title="groups" value={data.groups} />
-        <Input
-          addonBefore="department"
-          title="department"
-          value={data.department}
-        />
+      <Space
+        direction="vertical"
+        style={{
+          userSelect: "none",
+          alignItems: "center",
+          margin: "20%",
+          marginTop: "10%",
+          marginBottom: "10%",
+        }}
+      >
+        <Avatar size={64} icon={<UserOutlined />} />
+        <Title>{data.name}</Title>
+        <Space direction="vertical" style={{ userSelect: "none" }}>
+          <Input addonBefore="email" title="email" value={data.email} />
+          <Input addonBefore="id" title="id" value={data.id} />
+          <Input addonBefore="groups" title="groups" value={data.groups} />
+          <Input
+            addonBefore="department"
+            title="department"
+            value={data.department}
+          />
+        </Space>
       </Space>
     </>
   );
