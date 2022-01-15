2521df8e943d961379fcf4e4084b3ac9940deb78
yihsin.yu <yihsin.yu@linecorp.com>
2022-01-15 13:47:54 +0800

    fix some bug of signIn and signUp

--- frontend/src/Containers/MainPage/mainPage.js
+++ frontend/src/Containers/MainPage/mainPage.js
@@ -1,29 +1,21 @@
-import DragAndDrop from "./dragAndDrop";
-import ProgressBar from "./progressBar";
 import DocumentPage from "./DocumentPage";
 import CreateWorkflow from "../CreateWorkflow/createworkflow";
 import { Button } from "antd";
 import { useState } from "react";
+const Welcome = () => {
+  return null;
+};
 const MainPage = ({ user }) => {
-  const [newWorkflow, setNewWorkflow] = useState(false);
+  const [page, setPage] = useState({ key: "welcome" });
   return (
     <>
-<<<<<<< HEAD
-      <DocumentPage user={user}/>
-=======
-      {newWorkflow === true ? (
-        <CreateWorkflow setNewWorkflow={setNewWorkflow} />
+      {page["key"] == "document" ? (
+        <DocumentPage setPage={setPage} document={page["document"]} />
+      ) : page["key"] == "createWorkflow" ? (
+        <CreateWorkflow setPage={setPage} document={page["document"]} />
       ) : (
-        <DocumentPage />
+        <Welcome />
       )}
-      <Button
-        type="primary"
-        onClick={() => setNewWorkflow(true)}
-        style={newWorkflow === true ? { display: "none" } : {}}
-      >
-        NEW
-      </Button>
->>>>>>> 8766e57 (First version of create workflow)
     </>
   );
 };
